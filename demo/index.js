9b89867de442b98bc6512ebc04a244c11902ba45
MohitGupta0424 <mohitgupta_0470@yahoo.com>
2019-05-21 18:08:17 -0400

    Initial commit

---
+++ demo/index.js
@@ -0,0 +1,24 @@
+var playerList = [
+{name: "player1", highScore: 1, ability: 8},
+{name: "player2", highScore: 1, ability: 7},
+{name: "player3", highScore: 1, ability: 6},
+{name: "player4", highScore: 1, ability: 5},
+{name: "player5", highScore: 1, ability: 4},
+{name: "player6", highScore: 1, ability: 3},
+{name: "player7", highScore: 1, ability: 2},
+{name: "player8", highScore: 1, ability: 1}
+];
+
+    for (var i = 0; i < playerList.length; i++) {
+            var player = document.getElementById("player" + (i + 1));
+            var playerscore = document.getElementById('player' + (i + 1) + "score")
+    var progress=Math.random();
+    progress=11*progress;
+    progress=Math.floor(progress);
+    playerList[i].ability=playerList[i].ability+progress;
+
+            //add players score to the table//
+
+            playerscore.innerText = playerList[i].ability;
+
+    }
